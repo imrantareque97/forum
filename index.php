100f9a189bcf59df627168d0ccfa10bb52af96c8
Imran Tareque <imrant97@vt.edu>
2018-07-06 17:47:21 -0400

    Commit #4 - Enhanced forum table

--- index.php
+++ index.php
@@ -3,32 +3,37 @@
   <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="themes/default/css/style.css">
   <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>Esports Forum </title>
-
+  <title>Esports Forum</title>
 </head>
-<div class ="headerfixedBar">
+
+<div class ="headerFixedBar">
   <div class ="row">
     <div class ="col-lg-12">
-    <div class ="pullLeft">
-      <a class ="headerLogoText" href="index.php" title="EsportsForum.com"><img class ="headerLogoImage" src="themes/default/images/logo.png">Forum</a>
-    </div>
 
-    <div class ="pullRight">
+      <div class ="pullLeft" style ="margin-top:5px;">
+      <a class ="headerLogoText" href="index.php" title="EsportsForum.com"><img class ="headerLogoImage" src="themes/default/images/logo.png">Forum</a>
+      </div>
+
+      <div class ="pullRight">
       <a class ="btn btn-primary themeButton" href="index.php?logout" title="EsportsForum.com">Logout</a>
+      </div>
+
     </div>
   </div>
-  </div>
+</div>
 
 <body>
+
+
   <div class ="container">
     <div class ="row">
-      <div class ="col-lg-12 pagenationBar forumModule forumMargin">
+      <div class ="col-lg-12 paginationBar forumModule forumMargin">
         <a href="#">Home</a> <!--i class ="fa fa-angle-right paginationArrow"></i> <a href="#">Category</a> <i class ="fa fa-angle-right paginationArrow"></i> <a href="#">Discussion</a -->
       </div>
     </div>
 
 
-    <div class ="row forumModule">
+    <div class ="row categoryModule">
       <div class ="col-lg-12 columnPad">
         <div class="pullLeft">
           <h1>Category Name</h1>
@@ -39,8 +44,8 @@
       </div>
     </div>
 
-    <div class ="row forumModule forumPad">
-      <table>
+    <div class ="row forumPad">
+      <table class="forumTable">
         <thead>
           <th>
             Discussion
@@ -48,13 +53,35 @@
           <th>
             Author
           </th>
-          <th>
+          <th class="pullRight">
             Replies
           </th>
         </thead>
-        <tr>
 
+        <tr class="forumModule">
+          <td>
+          <a href="#">  Discussion name here </a>
+          </td>
+          <td>
+            <a href="#">Imran</a>
+          </td>
+          <td class="pullRight">
+            10
+          </td>
         </tr>
+
+        <tr class="forumModule">
+          <td>
+          <a href="#">  Discussion name here </a>
+          </td>
+          <td>
+            <a href="#">Imran</a>
+          </td>
+          <td class="pullRight">
+            10
+          </td>
+        </tr>
+
       </table>
     </div>
 
@@ -63,5 +90,5 @@
 </body>
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" type="text/javascript"></script>
-<scriptsrc="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 </html>
